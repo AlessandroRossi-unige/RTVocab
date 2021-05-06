e9ac5f6a1cbfa0748887632b7b6884ec40fae2eb
AlessandroRossi-unige <72498335+AlessandroRossi-unige@users.noreply.github.com>
2021-05-06 12:20:28 +0200

    Second commit, added dependencies

--- app/src/main/java/com/example/rtvocab/MainActivity.java
+++ app/src/main/java/com/example/rtvocab/MainActivity.java
@@ -1,8 +1,21 @@
 package com.example.rtvocab;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.microsoft.azure.cognitiveservices.vision.computervision.*;
+import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionImpl;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import com.google.gson.*;
+import com.squareup.okhttp.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
