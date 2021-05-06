a79e8ba6bf8c6d35f5594696568e9635374bbbf0
AlessandroRossi-unige <72498335+AlessandroRossi-unige@users.noreply.github.com>
2021-05-06 14:52:08 +0200

    Fixed layout

---
+++ app/src/main/java/com/example/rtvocab/ActivityVision.java
@@ -0,0 +1,14 @@
+package com.example.rtvocab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityVision extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vision);
+    }
+}
--- app/src/main/java/com/example/rtvocab/MainActivity.java
+++ app/src/main/java/com/example/rtvocab/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.rtvocab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.microsoft.azure.cognitiveservices.vision.computervision.*;
 import com.microsoft.azure.cognitiveservices.vision.computervision.implementation.ComputerVisionImpl;
 import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
@@ -19,9 +24,24 @@ import com.squareup.okhttp.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_getAnalysis = null;
+    Button btn_Translate = null;
+    RecyclerView rv_AnalysisResults = null;
+    RecyclerView rv_TranslateResults = null;
+    EditText et_datainput = null;
+    EditText et_Translate = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_getAnalysis = findViewById(R.id.btn_getAnalysis);
+        btn_Translate = findViewById(R.id.btn_Translate);
+        et_datainput = findViewById(R.id.et_dataInput);
+        et_Translate = findViewById(R.id.et_Translate);
+        rv_AnalysisResults = findViewById(R.id.rv_AnalysisResults);
+        rv_TranslateResults = findViewById(R.id.rv_TranslateResults);
     }
 }
